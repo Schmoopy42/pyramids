3757f5d119c4370bd59d1ae2faab3460aba2fa3c
Jerry <jerry.wasdyke@gmail.com>
2020-06-29 14:05:55 -0400

    rearrange states and add skeleton for onClick

--- pyramids-react/src/index.js
+++ pyramids-react/src/index.js
@@ -2,25 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+function Square(props) {
+  return(
+    <button
+      className="square"
+      onClick={props.onClick}
+    >
+      {props.value}
+    </button>
+  );
+}
+
 class Board extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { 
-      //squares: Array(9).fill(1),
-      //squares: [null, null, null, 3, 3, 3, null, null, null],
-      squares: [
-        [[null], [null], [null]],
-        [[3,2,1], [3,2,1], [3,2,1]],
-        [[null], [null], [null]],
-      ],
-    };
-  }
 
   renderSquare(i) {
     return (
-    <button className="square">
-      {i}
-    </button>
+      <Square
+        value={i}
+        onClick={() => this.props.onClick()}
+      />
     );
   }
 
@@ -28,19 +28,19 @@ class Board extends React.Component {
     return (
       <div>
         <div className="board-row">
-          {this.renderSquare(this.state.squares[0][0])}
-          {this.renderSquare(this.state.squares[0][1])}
-          {this.renderSquare(this.state.squares[0][2])}
+          {this.renderSquare(this.props.squares[0][0])}
+          {this.renderSquare(this.props.squares[0][1])}
+          {this.renderSquare(this.props.squares[0][2])}
         </div>
         <div className="board-row">
-          {this.renderSquare(this.state.squares[1][0])}
-          {this.renderSquare(this.state.squares[1][1])}
-          {this.renderSquare(this.state.squares[1][2])}
+          {this.renderSquare(this.props.squares[1][0])}
+          {this.renderSquare(this.props.squares[1][1])}
+          {this.renderSquare(this.props.squares[1][2])}
         </div>
         <div className="board-row">
-          {this.renderSquare(this.state.squares[2][0])}
-          {this.renderSquare(this.state.squares[2][1])}
-          {this.renderSquare(this.state.squares[2][2])}
+          {this.renderSquare(this.props.squares[2][0])}
+          {this.renderSquare(this.props.squares[2][1])}
+          {this.renderSquare(this.props.squares[2][2])}
         </div>
       </div>
     );
@@ -49,8 +49,31 @@ class Board extends React.Component {
 }
 
 class Game extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      squares: [
+        [[null], [null], [null]],
+        [[3,2,1], [3,2,1], [3,2,1]],
+        [[null], [null], [null]],
+      ],
+      p1isNext: true,
+      pyramidsPickedUp: [],
+    };
+  }
+
+  handleClick() {
+    /*  this will be used to select and move
+        pieces
+    */
+    this.setState({pyramidsPickedUp: [2]});
+  }
+
   render() {
-    return <Board />
+    return <Board 
+      squares={this.state.squares}
+      onClick={() => this.handleClick()}
+    />
   }
 }
 
