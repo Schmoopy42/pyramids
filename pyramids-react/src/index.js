add43c67361aeda8f364339e35200fa55abd38e4
Jerry <jerry.wasdyke@gmail.com>
2020-07-27 21:53:11 -0400

    add game state on top, stop picking up empty squares

--- pyramids-react/src/index.js
+++ pyramids-react/src/index.js
@@ -13,6 +13,12 @@ function Square(props) {
   );
 }
 
+function StatusBoard(props) {
+  return (
+    <h3>{props.player} has {props.movesLeft} moves left</h3>
+  );
+}
+
 class Board extends React.Component {
 
   renderSquare(y, x) {
@@ -59,23 +65,73 @@ class Game extends React.Component {
       p1isNext: true,
       pyramidsPickedUp: [],
       firstClick: true,
+      movesLeft: 3,
     };
+  }
+
+  rollPyramidDie() {
+    /*
+      Possible outcomes:
+        1
+        2
+        3
+        1,2
+        1,3
+        2,3
+    */
+    switch(Math.floor(Math.random() * 6) + 1) {
+      default:
+        console.log("something went wrong");
+        break;
+      case 1:
+        return [1];
+        break;
+      case 2:
+        return [2];
+        break;
+      case 3:
+        return [3];
+        break;
+      case 4:
+        return [1, 2];
+        break;
+      case 5:
+        return [1, 3];
+        break;
+      case 6:
+        return [2, 3];
+    }
   }
 
   grabPiece(y, x) {
     let currentPickedUp = this.state.pyramidsPickedUp;
-    currentPickedUp.push(this.state.squares[y][x].pop());
+    if (this.state.squares[y][x].length === 0) {
+      return;
+    } else {
+      currentPickedUp.push(this.state.squares[y][x].pop());
+      this.setState({
+        pyramidsPickedUp: currentPickedUp,
+      });
+    }
+  }
+
+  movePyramids(x,y){
+    //append this.state.pyramidsPickedUp to this.state.squares[y][x]
+    this.state.pyramidsPickedUp.reverse().forEach((item, array) => {
+      this.state.squares[y][x].push(item);
+    });
     this.setState({
-      pyramidsPickedUp: currentPickedUp,
+      pyramidsPickedUp: [],
     });
   }
 
   // pick up and move pieces
   handleClick(y, x) {
-/*  if (this.state.squares[y][x].length === 0 ) {
-      return;
-    } */
     if (this.state.firstClick) {
+      // if the square is empty, do nothing because you can't pick up nothing
+      if (this.state.squares[y][x].length === 0) {
+        return;
+      }
       this.setState({
         fromSquare: {
           "y": y,
@@ -90,7 +146,7 @@ class Game extends React.Component {
         this.grabPiece(y, x);
       } else {
         // if not, it's the toSquare
-        console.log("move pyramids");
+        this.movePyramids(x, y);
       }
     }
 
@@ -104,10 +160,18 @@ class Game extends React.Component {
   }
 
   render() {
-    return <Board 
-      squares={this.state.squares}
-      onClick={(y, x) => this.handleClick(y, x)}
-    />
+    return(
+      <div>
+        <StatusBoard 
+          player={this.state.p1IsNext ? "Player 1" : "Player 2"}
+          movesLeft={this.state.movesLeft}
+        />
+        <Board 
+          squares={this.state.squares}
+          onClick={(y, x) => this.handleClick(y, x)}
+        />
+      </div>
+    );
   }
 }
 
